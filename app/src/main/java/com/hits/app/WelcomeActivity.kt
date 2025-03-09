0123d3a294c58263813170328591b0002ca0cf98
Tolkacheva Maria <141151654+masahatolk@users.noreply.github.com>
2025-03-09 22:17:49 +0700

    fixed sth

--- app/src/main/java/com/hits/app/WelcomeActivity.kt
+++ app/src/main/java/com/hits/app/WelcomeActivity.kt
@@ -2,7 +2,6 @@ package com.hits.app
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.hits.app.databinding.ActivityWelcomeBinding
 
@@ -12,11 +11,6 @@ class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        );
 
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
